d02665ff22150a1bf1f930a35b633427d42118fa
sungh123 <hsh1352@donga.ac.kr>
2021-07-23 15:03:33 +0900

    JSX문법공부-1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import JsxTest from "./test/JsxTest";
+
+
+function App() {
+  // 1. JSX는 return 시에 하나의 dom만 리턴할 수 있다.
+  return  <div>
+    <JsxTest />
+  </div>;
+}
+
+export default App;
---
+++ src/test/JsxTest.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+let num = 10;
+let user = {
+    id: 1,
+    username: "ssar",
+    email:"ssar@nate.com"
+};
+
+// 배열 안에 오브젝트 세 개가 들어간 것
+let users = [
+    {
+        id: 1,
+        username: "ssar",
+        email:"ssar@nate.com"
+    },
+    {
+        id: 2,
+        username: "cos",
+        email:"cos@nate.com"
+    },
+    {
+        id: 3,
+        username: "love",
+        email:"love@nate.com"
+    }
+];
+function JsxTest() {
+    return (
+        <div>
+            <h1>JSX 문법 예제 {num}</h1>
+            <hr />
+            <div>
+                <div>3. 삼항 연산자 - IF를 사용할 수 없다. </div>
+                {num === 10 ? "ok" : "fail"}
+            </div>
+            <hr />
+            <div>
+                번호 : {user.id} <br />
+                유저네임 : {user.username} <br />
+                이메일 : {user.email} <br />
+            </div>
+            <hr />
+            <div>
+                {/* {users[0].username} */}
+                 
+            </div>
+        </div>
+    );
+}
+
+//2. export는 자바의 public과 같다
+export default JsxTest;
